1e8c44464091fab015f6d46552dcf77dd77800e4
avandokkum <arjanvandokkum@gmail.com>
2025-03-11 15:42:56 +0100

    test PC build

--- src/main/java/com/webcanis/Application.java
+++ src/main/java/com/webcanis/Application.java
@@ -2,12 +2,11 @@ package com.webcanis;
 
 public class Application {
     public static void main(String[] args){
+
         ImageService imageService = new ImageService();
         imageService.createOutputFolder();
         imageService.readImagesRefactor();
+
     }
-
-
-
 }
 
--- src/main/java/com/webcanis/PhilHarveyExifTool.java
+++ src/main/java/com/webcanis/PhilHarveyExifTool.java
@@ -2,4 +2,7 @@ package com.webcanis;
 
 public class PhilHarveyExifTool {
     // add PhilHarveyExifTool next to using DrewMetaDataExtractor
+
+    // test:
+    // https://exiftool.org/
 }
